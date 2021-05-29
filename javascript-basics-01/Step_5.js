28e821e6d4b4c2b113783a37a050e3a2051411a8
MariaJans <mariapjansian@gmail.com>
2021-05-29 17:26:32 +0300

    Completed-Step5

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,4 +1,4 @@
-function multiply() {
+function multiplication() {
     var numb1 = document.getElementById("first_number").value;
     var numb2 = document.getElementById("second_number").value;
     var result = numb1 * numb2;

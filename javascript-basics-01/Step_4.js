65002bb31feabcf4cf6c9f1314e7bed6641b1087
MariaJans <mariapjansian@gmail.com>
2021-05-29 13:10:55 +0300

    Completed-Step-4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name = window.prompt ("Enter your name");
+var surname= window.prompt ("Enter Surname");
+var city= window.prompt ("Enter your city");
+
+window.alert("Name : " + name +"\n"+ "Surname : " + surname +"\n"+ "City : " + city);

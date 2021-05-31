1df57cddd1733c3d3e1a26f46999efd6320d38d8
MariaJans <mariapjansian@gmail.com>
2021-05-31 04:10:41 +0300

    Completed-JavaScript-basic3-Step2

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,8 @@
+function showhide (paragraph){
+    if(paragraph==true){
+        document.getElementById("texte").style.visibility="visible";
+    }else{
+            document.getElementById('texte').style.visibility="hidden";
+        }
+    }
+    
